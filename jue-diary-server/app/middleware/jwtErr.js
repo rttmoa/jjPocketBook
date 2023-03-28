5a518650e49269e8edab467c39380091e811926c
rttmoa <rttmoa@gmail.com>
2023-03-28 15:49:43 +0800

    1

--- jue-diary-server/app/middleware/jwtErr.js
+++ jue-diary-server/app/middleware/jwtErr.js
@@ -1,3 +1,4 @@
+/* eslint-disable arrow-parens */
 'use strict';
 
 
@@ -5,13 +6,13 @@ module.exports = (options) => {
   // 首先中间件默认抛出一个函数，该函数返回一个异步方法 jwtErr，jewErr 方法有两个参数 ctx 是上下文，可以在 ctx 中拿到全局对象 app
   return async function jwtErr(ctx, next) {
     const token = ctx.request.header.authorization; // 若是没有 token，返回的是 null 字符串
-    let decode
-    if(token != 'null' && token) { // 如果是存在且有效
-      try {   
+    let decode;
+    if(token !== 'null' && token) { // 如果是存在且有效
+      try {
         decode = ctx.app.jwt.verify(token, options.secret); // 验证token
         await next(); // 继续执行后续的接口逻辑
       } catch (error) {
-        console.log('error', error)
+        console.log('error', error);
         ctx.status = 200;
         ctx.body = {
           msg: 'token已过期, 请重新登录',
